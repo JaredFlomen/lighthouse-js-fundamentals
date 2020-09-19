66aa1861aca5fd6a727a0fa4cc0462f2ca6ec3de
Jared <jaredflomen@gmail.com>
2020-09-19 17:09:46 +0000

    Checking if a number is odd

---
+++ odd.js
@@ -0,0 +1,13 @@
+function isOdd (num) {
+  if (num % 2 !== 0){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("10 is odd: " + isOdd(10));
+console.log("11 is odd: " + isOdd(11));
